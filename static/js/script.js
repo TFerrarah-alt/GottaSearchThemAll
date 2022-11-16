4a46fb9d89501cbad191e6eb298ea6e0f0e2bae6
sonoMinni <84858564+sonoMinni@users.noreply.github.com>
2022-11-16 16:48:12 +0100

    #34hxyw9 tolto obrobrio di position absolute

--- static/js/script.js
+++
@@ -1,9 +0,0 @@
-var inputField = document.querySelectorAll('.input-field');
-inputField.addEventListener('event', function() {
-    if(inputField.keycode === 13 && inputField.value === '') {
-        inputField.classlist.add('bounce')
-    }
-});
-
-
-
